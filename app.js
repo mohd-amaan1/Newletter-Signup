033da7b2528003b78bfce78c80ae377e5526a970
Mohd Amaan <118156938+mohdamaan069@users.noreply.github.com>
2024-01-15 20:22:59 +0530

    Update app.js

--- app.js
+++ app.js
@@ -72,6 +72,6 @@ app.post("/failure", (req,res) => {
     res.redirect("/");
 })
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log("Server is up and running!");
 });
